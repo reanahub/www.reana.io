b9a1ee59f4bf36ea496f276331680a6890d15f02
Diego Rodriguez <diego.rodriguez@cern.ch>
2020-09-22 13:19:33 +0200

    tests: check for broken links

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+echo '==> [INFO] Checking for broken links...'
+awesome_bot --allow-dupe --skip-save-results --allow-redirect --white-list https://reana.cern.ch **/*.html
+echo '==> [INFO] Testing Docker image build...'
 docker build -t reanahub/wwwreanaio .
